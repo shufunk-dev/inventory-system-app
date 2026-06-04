8722541aeef4f36ffa4fbc3c6f52cb5db6cfd490
Shufunk <shufunk@gmail.com>
2026-06-04 08:28:36 -0400

    Delete Archive directory
    These weren't supposed to be uploaded but it's ok. I just don't need them here for accidental upload and waste of space for others to download my junk test data

--- Archive/development_scripts/test_sqlite.js
+++
@@ -1,1 +0,0 @@
-const fs = require('fs'); try { require('better-sqlite3'); fs.writeFileSync('sqlite_test.log', 'SUCCESS!'); } catch (e) { fs.writeFileSync('sqlite_test.log', e.stack); }
--- Archive/development_scripts/test_vision.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-const fs = require('fs');
-
-async function testVision() {
-  try {
-    const fileBuffer = fs.readFileSync('public/uploads/test.jpg'); // need a test image
-  } catch(e) {
-  }
-}
